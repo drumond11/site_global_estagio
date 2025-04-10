80dc0442122fb54a16b2eef2c4fa5af969caea4f
Luis Ferreira <af7177360@gmail.com>
2025-04-10 15:01:04 +0100

    Card Consultor Elite adicionado e formatação dos outros card's

--- carreira/index.php
+++ carreira/index.php
@@ -2,28 +2,52 @@
 require '../views/head.php';
 require '../views/menu.php';
 ?>
-<link rel="stylesheet" href="carreira.css">
+<link rel="stylesheet" href="estilo.css">
 <div class="tit1">
     <h1 class="tit2">Escolha o seu plano</h1>
 </div> 
 <br>
-<div class="container">
-    <div class="pricing-table">
-        <div class="pricing-card">
-            <h2>Consultor Prime</h2>
-            <ul>
-                <li>&bull; <span>85%</span> comissão de venda.</li>
-                <li>&bull; <span>90%</span> comissão de arrendamento.</li>
-            </ul>
+<div class="container my-5">
+  <div class="row justify-content-start gx-4">
+    
+    <div class="col-md-4 mb-4">
+      <div class="card shadow h-100" style="min-width: 340px;">
+        <div class="card-body">
+          <h2 class="card-title">Consultor Elite</h2><br>
+          <span class="fs-1">100%</span> <span class="mb-2">Comissão de venda e arrendamento.</span>
+          <ul class="list-unstyled">
+            <li>• 95€ por Mês + IVA.</li><bR>
+            <li>• 350€ por transação + IVA.</li>
+          </ul>
         </div>
-        <div class="pricing-card">
-            <h2>Consultor Parceiro</h2>
-            <ul>
-                <li>&bull; <span>65%</span> comissão de venda e arrendamento.</li>
-                <li>&bull; Inclui todo o suporte (crm/jurídico/contabilidade/vídeo/fotografia).</li>
-            </ul>
-        </div>
+      </div>
     </div>
+
+    <div class="col-md-4 mb-4">
+  <div class="card shadow h-100" style="min-width: 340px;">
+    <div class="card-body">
+      <h2 class="card-title">Consultor Prime</h2><br>
+      <span class="fs-1">85%</span> <span class="mb-2">Comissão de venda.</span><br><br>
+      <span class="fs-1">90%</span> <span class="mb-2">Comissão de arrendamento.</span>
+    </div>
+  </div>
+</div>
+
+    <div class="col-md-4 mb-4">
+      <div class="card shadow h-100" style="min-width: 340px;">
+        <div class="card-body">
+          <h2 class="card-title">Consultor Parceiro</h2><br>
+          <span class="fs-1">65%</span> <span class="mb-2">Comissão de venda e arrendamento.</span>
+          <br>
+          <ul class="list-unstyled"><br>
+            <li>• Inclui todo o suporte crm/jurídico/contabilidade/vídeo/fotografia</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
     <div class="tit3">
         &bull;<span> 5%</span> de rede.<br>
         &bull; Fee anual: 300+IVA ou fee mensal 10+IVA.<br>
@@ -37,7 +61,7 @@ require '../views/menu.php';
         &bull; Suporte jurídico (CPCV/outros documentos).<br>
         &bull; Partilha de leads (clientes vendedores/clientes compradores) da empresa 20% para a empresa da comissão do consultor.
     </div>
-</div>
+
 <?php
 require '../views/footer.php';
 ?>
