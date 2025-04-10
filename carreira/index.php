c6eed8cd8a4363969f829374d722b5457c1f5d60
drumond11 <drumond11@gmail.com>
2025-04-10 17:21:02 +0100

    alterar texo e espaçamentos

--- carreira/index.php
+++ carreira/index.php
@@ -16,7 +16,7 @@ require '../views/menu.php';
           <h2 class="card-title">Consultor Elite</h2><br>
           <span class="fs-1">100%</span> <span class="mb-2">Comissão de venda e arrendamento.</span>
           <ul class="list-unstyled">
-            <li>• 95€ por Mês + IVA.</li><bR>
+            <li>• 95€ por Mês + IVA.</li>
             <li>• 350€ por transação + IVA.</li>
           </ul>
         </div>
@@ -27,7 +27,7 @@ require '../views/menu.php';
   <div class="card shadow h-100" style="min-width: 340px;">
     <div class="card-body">
       <h2 class="card-title">Consultor Prime</h2><br>
-      <span class="fs-1">85%</span> <span class="mb-2">Comissão de venda.</span><br><br>
+      <span class="fs-1">85%</span> <span class="mb-2">Comissão de venda.</span><br>
       <span class="fs-1">90%</span> <span class="mb-2">Comissão de arrendamento.</span>
     </div>
   </div>
@@ -37,9 +37,8 @@ require '../views/menu.php';
       <div class="card shadow h-100" style="min-width: 340px;">
         <div class="card-body">
           <h2 class="card-title">Consultor Parceiro</h2><br>
-          <span class="fs-1">65%</span> <span class="mb-2">Comissão de venda e arrendamento.</span>
-          <br>
-          <ul class="list-unstyled"><br>
+          <span class="fs-1">65%</span> <span class="mb-2">Comissão de venda e arrendamento.</span><br>
+          <ul class="list-unstyled">
             <li>• Inclui todo o suporte crm/jurídico/contabilidade/vídeo/fotografia</li>
           </ul>
         </div>
@@ -50,7 +49,7 @@ require '../views/menu.php';
 </div>
     <div class="tit3">
         &bull;<span> 5%</span> de rede.<br>
-        &bull; Fee anual: 300+IVA ou fee mensal 10+IVA.<br>
+        &bull; Fee anual: 300+IVA ou fee mensal 10+IVA (Somente para Consultor Prime/Consultor Parceiro).<br>
         &bull; Sala de reuniões privada: Preço por meia hora 3,50 + IVA €.<br>
         &bull; Sala de reuniões pública: gratuito mediante a disponibilidade.<br>
         &bull; Impressão Preto/branco 0,09+IVA por página €.<br>
@@ -58,7 +57,7 @@ require '../views/menu.php';
         &bull; Encadernações em espiral (inclui capa e contra-capa) 1,00+IVA €.<br>
         &bull; Se o negócio total for na rede da empresa - angariador recebe mais 1%.<br>
         &bull; Antes de colocar no mercado e comunicar à empresa recebe 65% da comissão do total da comissão.<br>
-        &bull; Suporte jurídico (CPCV/outros documentos).<br>
+        &bull; Suporte jurídico (CPCV/outros documentos) (Somente para Consultor Prime/Consultor Parceiro).<br>
         &bull; Partilha de leads (clientes vendedores/clientes compradores) da empresa 20% para a empresa da comissão do consultor.
     </div>
 
